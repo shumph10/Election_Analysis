b177a9b55c9c573a3f093efd25cbccf54f4440c7
shumph10 <sarahhumphrey2016@outlook.com>
2022-03-24 16:03:59 -0400

    completed election audit and printed winner info

--- PyPoll.py
+++ PyPoll.py
@@ -6,6 +6,17 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#1. init a total vote counter
+total_votes = 0
+#declare a list for candiate name option, 2 bc its in row 3 so index 2
+candidate_options = []
+#declare an empty dictionary for the candidates votes
+candidate_votes = {}
+#declare variables for winning candidate, winning count and percent
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -14,9 +25,62 @@ with open(file_to_load) as election_data:
 
     #Read and print the header row
     headers = next(file_reader)
-    print(headers)
-#1. The total # of votes cast
+    
+    #Print each row in the CSV file
+    for row in file_reader:
+            #add to the total vote count
+            total_votes += 1
+
+            #print the candidate name from each row
+            candidate_name = row[2]
+
+            #add the candidate name to the candidate list
+            if candidate_name not in candidate_options:
+                #add it to the list of candidates
+                candidate_options.append(candidate_name)
+
+                #2. Begin tracking that candidate's vote count using format to get the key from a dictionary
+                candidate_votes[candidate_name] = 0
+
+            candidate_votes[candidate_name] +=1
+
+    #det the % votes for each candidate by looping through the counts
+    #iterate through the candidate list
+    for candidate_name in candidate_votes:
+        #2. retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        #3 calc the % of votes - float bc want to change from int data type to float to get decimals
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #4. print the candidate name and % of votes w/ 2 decimal places
+        print(f'{candidate_name}: received {vote_percentage:.2f}% of the vote.')
+
+    #print each candidates name, vote count, and % of votes
+        print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+
+        #det winning vote count and candidate
+        #determine if the votes are greater than the winning count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            #if true set winning count = votes(bc votes assigned to candidate b4) and winning % = vote %
+            winning_count = votes
+            winning_percentage = vote_percentage
+            #set the winning candidate = to candidates name
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"---------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"---------------------------\n")
+    print(winning_candidate_summary)
+
+print(total_votes)
+print(candidate_options)
+print(candidate_votes)
+print(winning_candidate)
+
 #2. A complete list of canidates who received votes
+
 #3. The percentage of votes each candidate won
 #4.The total number of votes each candidate won
 #5. The winner of the election based on popular vote
--- Python_practice.py
+++ Python_practice.py
@@ -1,3 +1,37 @@
 counties = ["Arapahoe","Denver","Jefferson"]
 if counties[1] == 'Denver':
     print(counties[1])
+
+#The data we need to retrieve
+#assign a variable for the file to load and the path
+#Indirect path file opening
+import csv
+import os
+file_to_load = os.path.join("Resources", "election_results.csv")
+#open the election results.csv using the with statement as the filename obk election_Data
+with open(file_to_load) as election_data:
+    print(election_data)
+
+    ##file_to_load = 'Resources\election_results.csv'
+
+#open the election results and read the file
+    ##election_data = open(file_to_load, 'r')
+
+#use with so you dont have to open/close
+    ##with open(file_to_load) as election_data:
+        ##print(election_data)
+#to do: perform analysis then close the file
+
+###Creating a file for results to be written
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+#use open statement to open the file as a text file
+    #use with statement so you dont have to open and vlose
+with open(file_to_save, "w") as txt_file:
+    txt_file.write("Counties in the Election\n--------\nAnapahoe\nDenver\nJefferson")
+
+
+#write some data to the file, #write() funct is from the os module
+
+#close the file
+
+
